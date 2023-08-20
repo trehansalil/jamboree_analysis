5a01bcc4a398231491ebe591ec2d913d3f3ac973
Salil Trehan <trehansalil1@gmail.com>
2023-08-20 12:48:24 +0530

    Basic Setup 3

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 git config --global user.name "Salil Trehan"
 git config --global user.email "trehansalil1@gmail.com"
 pip install -r requirements.txt> logs/installer.log 2>&1&
+wget -O data/jamboree_dataset.csv https://d2beiqkhq929f0.cloudfront.net/public_assets/assets/000/001/839/original/Jamboree_Admission.csv
